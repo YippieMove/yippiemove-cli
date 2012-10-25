41bf3d9f695a3e6534d87d34fc286dfb1a906a7f
Zachary Jones <zacharytamas@gmail.com>
2012-10-25 13:52:28 -0400

    Only a single payment is allowed now, the Order resource was changed to represent this.

--- yippiemove.py
+++ yippiemove.py
@@ -454,7 +454,7 @@ def status(object_type, object_id):
         print("  Status of Order #%d" % order['id'])
         print("=" * 64)
         print("  Owner: %s" % user['email'])
-        print("  This order has been paid for." if len(order['payments']) else "  This order has not been paid for.")
+        print("  This order has been paid for." if order['payment'] else "  This order has not been paid for.")
         print("")
         print("  Move Jobs on this Order:")
 
